f15f24e58587e834648fe54ad4d86b0d7ff01d19
Ayush9545 <ayushgotefode@gmail.com>
2026-01-05 13:30:10 +0530

    Started the code for transfer

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 import express from "express";
+import pg from "pg";
+
 const app = express();
-const post = 3000;
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.json({ 
+        status: "Live",  
+        message: "PayGuard Bank API is running smoothly 🚀" 
+    });
+    console.log("This is home page.")
+})
+
+app.post("/transfer", async (req, res)=>{
+    const sender_id = req.body.sender_id;
+    const receiver_id = req.body.receiver_id;
+    const amount=req.body.amount;
+    const client = await db.connect();
+    try {
+        await client.query('BEGIN');
+        await client.query('COMMIT');
+        res.json({ message: "Transaction Started!" });
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.error("Transaction Error:", error);
+        res.status(500).json({message: "Transaction Failed"});
+    } finally {
+        client.release();
+    }
+})
 
 app.listen(port, ()=>{
-    console.log(`Server is running on ${post}`);
+    console.log(`Server is running on ${port}.`);
 })
