b9fa3e775d96f13a2ee3658b09e0b94b315eb64c
Ayush9545 <ayushgotefode@gmail.com>
2026-06-25 22:40:04 +0530

    Implement protected routes

--- payguard-frontend/src/App.jsx
+++ payguard-frontend/src/App.jsx
@@ -3,14 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App(){
+function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
